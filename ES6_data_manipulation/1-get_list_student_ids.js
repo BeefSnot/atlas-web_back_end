bb43021fb756d7cbae5146aae7dd5292570243c0
James <james@jameshamby.me>
2025-03-12 13:21:40 -0500

    test

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,7 @@
 export default function getListStudentIds(studentList) {
-    if (!Array.isArray(studentList)) {
-      const emptylist = [];
-      return emptylist;
-    }
-  
-    const newArray = studentList.map((item) => item.id);
-  
-    return newArray;
+  if (!Array.isArray(studentList)) {
+    return [];
   }
-  
+
+  return studentList.map((item) => item.id);
+}
