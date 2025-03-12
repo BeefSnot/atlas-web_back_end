a0d11047ccb96d9c830d9e74614fdc872d111d49
James <james@jameshamby.me>
2025-03-12 13:54:07 -0500

    4 and 5

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const updatedStudentList = studentList.map((item) => {
+    const newGrade = newGrades.find((grade) => grade.studentId === item.id);
+    const grade = newGrade ? newGrade.grade : 'N/A';
+    return { ...item, grade };
+  });
+
+  const sortedStudentList = updatedStudentList.filter((item) => item.location === city);
+
+  return sortedStudentList;
+}
