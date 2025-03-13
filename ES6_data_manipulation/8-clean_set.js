76d7529c2454fca84c9c2f7abe897cab8cd9bef8
James <james@jameshamby.me>
2025-03-12 19:43:17 -0500

    pushing

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result += `${item.substring(startString.length)}-`;
+    }
+  }
+
+  return result.slice(0, -1);
+}
