a2ea8a2d45a8fbe906fafb56a12a05193de39d10
James <james.hamby@atlasschool.com>
2025-05-10 16:36:23 -0500

    Fixing pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -22,9 +22,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     results = []
-    
+
     for completed in asyncio.as_completed(tasks):
         result = await completed
         results.append(result)
-        
+
     return sorted(results)
