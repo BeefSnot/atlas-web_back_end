2261f33c3a031ff3eefa873ff7dc023b732e528f
James <james.hamby@atlasschool.com>
2025-05-26 13:11:30 -0500

    Last bit of this, needing to push so I can upload next project.

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""LRU caching system implementation"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU Cache that discards least recently used items"""
+
+    def __init__(self):
+        """Initialize cache system"""
+        super().__init__()
+        self.access_order = []  # Track access history
+
+    def put(self, key, item):
+        """Add item to cache with LRU replacement policy"""
+        if not key or not item:
+            return
+
+        # Update access history for existing keys
+        if key in self.access_order:
+            self.access_order.remove(key)
+        
+        # Append to track as most recently used
+        self.access_order.append(key)
+        
+        # Handle eviction if cache is full
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            # Remove least recently used item (front of list)
+            lru = self.access_order.pop(0)
+            del self.cache_data[lru]
+            print(f"DISCARD: {lru}")
+            
+        # Store item
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve item and update access history"""
+        if not key or key not in self.cache_data:
+            return None
+            
+        # Update access history
+        self.access_order.remove(key)
+        self.access_order.append(key)
+        
+        return self.cache_data[key]
---
+++ caching/4-mru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""MRU caching system implementation"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU Cache that removes most recently used items when full"""
+
+    def __init__(self):
+        """Set up cache tracking"""
+        super().__init__()
+        self.usage_list = []  # Track item usage
+
+    def put(self, key, item):
+        """Store item using MRU eviction strategy"""
+        if key is None or item is None:
+            return
+            
+        # If updating existing item, refresh its position
+        if key in self.usage_list:
+            self.usage_list.remove(key)
+            
+        # Make this the most recently used item
+        self.usage_list.append(key)
+        
+        # Handle cache overflow
+        if key not in self.cache_data and len(self.cache_data) >= self.MAX_ITEMS:
+            # The most recently used item is the second-to-last in our list
+            # (last is the current one we're adding)
+            mru = self.usage_list[-2]
+            self.cache_data.pop(mru)
+            self.usage_list.remove(mru)
+            print(f"DISCARD: {mru}")
+            
+        # Add to cache
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item and update usage tracking"""
+        if key not in self.cache_data:
+            return None
+            
+        # Update usage tracking
+        self.usage_list.remove(key)
+        self.usage_list.append(key)
+        
+        return self.cache_data[key]
