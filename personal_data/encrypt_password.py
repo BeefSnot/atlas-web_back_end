9beda8a592cf8d0bfb3ab59d8b72609a888c9dd5
James <james.hamby@atlasschool.com>
2025-05-31 21:55:59 -0500

    test

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -38,4 +38,3 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     # Check if the password matches the hash
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
