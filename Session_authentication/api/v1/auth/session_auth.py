aa5d8d27abfd6df092e75a62846dc6ab55faea1c
James <james.hamby@atlasschool.com>
2025-06-04 13:48:29 -0500

    first push

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Session authentication module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class that inherits from Auth"""
+    pass
