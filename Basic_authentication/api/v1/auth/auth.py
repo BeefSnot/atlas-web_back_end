2c1e3669b066eb28d43fb51cee3ae7fd8dff95e6
James <james.hamby@atlasschool.com>
2025-06-04 11:50:54 -0500

    d

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -31,9 +31,14 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """
         Gets the authorization header from the request
-        Returns None for now
         """
-        return None
+        if request is None:
+            return None
+            
+        if 'Authorization' not in request.headers:
+            return None
+            
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
