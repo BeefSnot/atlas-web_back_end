39519c3154ab467022bd35ab4697bb8fba5f1bcd
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-06-11 16:14:57 +0000

    task 7 fix

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- Creates a stored procedure that computes and stores the average score for a student
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN p_user_id INT
 )
 BEGIN
     DECLARE avg_score FLOAT;
@@ -10,11 +10,11 @@ BEGIN
     SELECT AVG(score) 
     INTO avg_score
     FROM corrections 
-    WHERE user_id = user_id;
+    WHERE user_id = p_user_id;
     
     -- Update the user's average_score
     UPDATE users 
     SET average_score = avg_score 
-    WHERE id = user_id;
+    WHERE id = p_user_id;
 END$$
 DELIMITER ;
