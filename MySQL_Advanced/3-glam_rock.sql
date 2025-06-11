574434c12de0095b41d7ad5e462ed2d77ac52011
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-06-11 15:51:03 +0000

    first part, pushing as i'm having connection issues.

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- A list of all bands with Glam rock as their main style
+SELECT
+    band_name, 
+    IFNULL(split, 2022) - formed as lifespan
+FROM
+    metal_bands
+WHERE
+    FIND_IN_SET('Glam rock', style)
+ORDER BY
+    lifespan DESC;
