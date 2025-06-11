fe06e8e848494aa77a41497edb9394fe1e3bfaae
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-06-11 15:52:07 +0000

    test fix

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
--- A list of all bands with Glam rock as their main style
+-- Lists all bands with Glam rock as their main style, ranked by longevity
 SELECT
     band_name, 
-    IFNULL(split, 2022) - formed as lifespan
+    IFNULL(split, 2024) - formed as lifespan
 FROM
     metal_bands
 WHERE
