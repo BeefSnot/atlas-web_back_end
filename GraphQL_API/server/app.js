e15676818326a08ee9f64452d68c7c197f371309
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-07-21 16:27:50 +0000

    graphql

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://jameshamby:BAUO958AUZAvXslx@cluster0.eqictx2.mongodb.net/graphql-db?retryWrites=true&w=majority');
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Database connection error:', err);
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
