c8e7d267ec09d364e4a7b41eb55afc1b1d02bd5d
James Hamby <161774551+BeefSnot@users.noreply.github.com>
2025-09-25 03:21:01 +0000

    fixing app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,17 +2,16 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-app.register_blueprint(app_views)
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 
+from os import getenv
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS
+import os
+from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.register_blueprint(app_views)
 
 
 @app.errorhandler(404)
