6b19239e3f228c51f62cadb05b71c932227a36f4
James Hamby <161774551+BeefSnot@users.noreply.github.com>
2025-09-25 03:07:50 +0000

    adding models

---
+++ Basic_authentication/api/v1/models/base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Base model for API v1."""
+
+class BaseModel:
+    """Base class for all models in API v1."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
+        pass
