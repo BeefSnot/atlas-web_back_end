7c66d640334666385fe4ac8859b62f9d41cb218d
James Hamby <161774551+BeefSnot@users.noreply.github.com>
2025-09-25 03:23:16 +0000

    File copy

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""SessionAuth module for Session Authentication."""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """SessionAuth class for handling Session Authentication."""
+    pass
